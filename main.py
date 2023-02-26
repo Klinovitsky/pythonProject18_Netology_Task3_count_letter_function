3af7d86e5db3a0b0000166cd27706612fc4e8402
Klinovitsky <klinovitsky@gmail.com>
2023-02-26 18:42:21 +0300

    Ver 2 (ready) X !=x

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-words_list = ["Xmas", "Xenomorph", "Reader", "Fool", "X-ray"]
+words_list = ["Xmas", "Xenomorph", "Reader", "Fool", "Xor", "Xenon"]
 print("Words in the list are: ", words_list)
-letter: str = input("Input the letter to find: ")
+letter = "X"
+print("Letter is: ", letter)
+#letter: str = input("Input the letter to find: ")
 
 def count_letter(words_list, letter):
-  print("Function is started")
-# Check the number of words
+  result = 0
+  # Check the number of words
   for word in words_list:
-    result = 0
-# Check the letter in the word
     if letter in word:
-        result = result + 1
-    print(word, letter, result)
-  # elif:
-  #   print("??")
-
+      print("LETTER cycle: ", word, letter, result)
+      result += 1
   return result
 
 number = count_letter(words_list, letter)
 print(f"The number of words with {letter} is: {number}")
 
-
-
-
-
+#print(count_letter(["Хmas", "Xenomorph", "Reader", "Fool", "X-ray"], "X"))
